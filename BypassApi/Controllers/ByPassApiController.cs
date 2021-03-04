74fa8f2a3e15836fbf941132eee99ad3e111c1cc
joaolsa <joaoluizdoss@gmail.com>
2021-03-04 18:23:18 -0300

    Add Id to request parameters

--- BypassApi/Controllers/ByPassApiController.cs
+++ BypassApi/Controllers/ByPassApiController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace BypassApi.Controllers
 {
-    class PopularMovie
+    public class Movie
     {
         public string title { get; set; }
     }
@@ -24,22 +25,21 @@ namespace BypassApi.Controllers
         }
 
         [HttpGet]
-        public async void Get()
+        public async Task<Movie> Get(int Id)
         {
-            using (var client = new HttpClient())
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri($"https://api.themoviedb.org/3/movie/{Id}?api_key=82e1a6e0bc65e50a5fd5ea2b171e83da");
+            //HTTP GET
+            var result = await client.GetAsync("");
+
+            Movie movie = null;
+            if (result.IsSuccessStatusCode)
             {
-                client.BaseAddress = new Uri("https://api.themoviedb.org/3/movie/550?api_key=82e1a6e0bc65e50a5fd5ea2b171e83da");
-                //HTTP GET
-                var result = await client.GetAsync("");
 
-                if (result.IsSuccessStatusCode)
-                {
-
-                    string responseBody = await result.Content.ReadAsStringAsync();
-                    var test = JsonSerializer.Deserialize<PopularMovie>(responseBody);
-                    Console.WriteLine(test);
-                }
+                string responseBody = await result.Content.ReadAsStringAsync();
+                movie = JsonSerializer.Deserialize<Movie>(responseBody);
             }
+            return movie;
         }
     }
 }
