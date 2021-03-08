8fe94338e94775723f88d9194c66d2a66270c023
joaolsa <joaoluizdoss@gmail.com>
2021-03-08 10:52:11 -0300

    Add authorization

--- BypassApi/Controllers/MovieController.cs
+++ BypassApi/Controllers/MovieController.cs
@@ -4,13 +4,14 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using BypassApi.Interfaces;
 using BypassApi.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BypassApi.Controllers
 {
 
     [ApiController]
-    [Route("/api")]
+    [Route("/api"), Authorize]
     public class MovieController : ControllerBase
     {
         private readonly IMovieAPIRepository _movieAPIRepository;
--- BypassApi/Startup.cs
+++ BypassApi/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using BypassApi.APIConnections;
 using BypassApi.Helpers;
 using BypassApi.Interfaces;
 using BypassApi.Repositories;
+using IdentityModel;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,6 +38,23 @@ namespace BypassApi
             services.AddScoped<IMovieAPIRepository, MovieAPIRepository>();
 
             services.AddSingleton<IStringHelpers, StringHelpers>();
+
+            // Setting OAuth2 configuration
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://jpproject-sso:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiSecret = "api-secret";
+                    options.ApiName = "report-api";
+                    options.RoleClaimType = JwtClaimTypes.Role;
+                    options.NameClaimType = JwtClaimTypes.Name;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +70,8 @@ namespace BypassApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
